5ba9f9ec8d4e6124c838f0ea860777417f521877
xiaomo-li <867416257@qq.com>
2024-03-20 18:43:52 +0800

    init

---
+++ p106/js/fix.js
@@ -0,0 +1,38 @@
+// 给根节点设置 id=root 即可
+
+$(function () {
+  $("#root").css("zoom", "normal");
+  let scale = document.body.clientWidth / 1080;
+  $("#root").css("zoom", scale);
+
+  window.addEventListener(
+    "resize",
+    (function () {
+      var timer;
+      return function () {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(function () {
+          $("#root").css("zoom", "normal");
+          let scale = document.body.clientWidth / 1080;
+          $("#root").css("zoom", scale);
+        }, 100);
+      };
+    })()
+  );
+
+  // 如果不是锚点跳转 则可以用下面这段代码实现平滑跳转
+  // window.onload = function () {
+  //     const targetElement = document.getElementById('buyBox'); // 获取跳转目标节点
+  //     const offsetTop = targetElement.offsetTop ;
+  //     $('.buy').click(function(){
+  //         window.scroll({
+  //             top: offsetTop * ($('div#root').css('zoom')) - 150,
+  //             behavior: 'smooth' // 使用 smooth 实现平滑滚动
+  //         });
+  //     })
+
+  //     // $('.buy').click(function(){
+  //     //     $('body,html').animate({'scrollTop':scrollNum - 300},500)
+  //     // })
+  // };
+});
